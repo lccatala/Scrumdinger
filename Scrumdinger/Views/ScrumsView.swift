66e1e59eeae88cf8ee84fa74de1ed7eecc978a2b
lccatala <alpasfly@gmail.com>
2023-11-05 02:02:03 +0100

    Display cards in List and set it as initial view for the app

---
+++ Scrumdinger/Views/ScrumsView.swift
@@ -0,0 +1,22 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Luis Carlos Catalá Martínez on 5/11/23.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    var body: some View {
+        List(scrums) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+        }
+    }
+}
+
+#Preview {
+    ScrumsView(scrums: DailyScrum.sampleData)
+}
